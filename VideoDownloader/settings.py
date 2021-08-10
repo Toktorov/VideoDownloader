ac349477dd3f2582e4a8ce1e01f93e2486ee6e5d
Toktorov <ktoktorov2005@gmail.com>
2021-08-10 16:06:02 +0600

    output information

--- VideoDownloader/settings.py
+++ VideoDownloader/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+
+    #apps
+    'ytdl',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'VideoDownloader.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
